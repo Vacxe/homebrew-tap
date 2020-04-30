de726d980fa15cea24dadfd8119c5d5a4b49943c
Vacxe <aksenov.kostya@gmail.com>
2020-04-29 17:52:52 -0700

    first commit

---
+++ Formula/googleplaycli.rb
@@ -0,0 +1,16 @@
+class GooglePlayCli < Formula
+  desc "Command line interface for Google play"
+  homepage "https://github.com/Vacxe/google-play-cli"
+  url "https://github.com/Vacxe/google-play-cli/releases/download/0.1/googleplaycli-0.1.zip"
+  sha256 "337b4c479b79a93961821617f9e3da5e8678e1579ea2fbfde54a6bbeeadb7b0f"
+
+  bottle :unneeded
+
+  depends_on :java => "1.8+"
+
+  def install
+    rm_f Dir["bin/*.bat"]
+    libexec.install %w[bin lib]
+    (bin/"googleplaycli").write_env_script libexec/"bin/googleplaycli", Language::Java.overridable_java_home_env
+  end
+end
