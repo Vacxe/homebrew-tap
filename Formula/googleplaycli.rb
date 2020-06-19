b9e85cab3a5123cb070578ea67122b966e02f19b
Konstantin Aksenov <aksenov.kostya@gmail.com>
2020-06-19 16:42:16 +1000

    Update googleplaycli.rb

--- Formula/googleplaycli.rb
+++ Formula/googleplaycli.rb
@@ -1,7 +1,7 @@
 class Googleplaycli < Formula
   desc "Command line interface for Google play"
-  homepage "https://github.com/Vacxe/google-play-cli"
-  url "https://github.com/Vacxe/google-play-cli/releases/download/0.1.3/google-play-cli-0.1.3.zip"
+  homepage "https://github.com/Vacxe/google-play-cli-kt"
+  url "https://github.com/Vacxe/google-play-cli-kt/releases/download/0.1.3/google-play-cli-0.1.3.zip"
   sha256 "1fc4a2e3c780f3e0881f7ce18e541ca4f237076b219588e3c0bdb9ae0f8507d9"
 
   bottle :unneeded
