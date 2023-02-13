8547667b63ab6313463f487003eaf90fd431db6d
Konstantin Aksenov <aksenov.kostya@gmail.com>
2023-02-13 21:51:38 +1000

    Update apkinfoextractor.rb

--- Formula/apkinfoextractor.rb
+++ Formula/apkinfoextractor.rb
@@ -4,10 +4,8 @@ class Apkinfoextractor < Formula
   url "https://github.com/Vacxe/apk-info-extractor/releases/download/0.1/apkinfoextractor-0.1.zip"
   sha256 "38d56e78d37f1f017f3bae3ae79b77f4bbf27c6b3ce7f177c754fe683a3eec1e"
 
-  bottle :unneeded
-
   depends_on "openjdk@11"
-
+  
   def install
     rm_f Dir["bin/*.bat"]
     libexec.install %w[bin lib]
