d20e82a68e832d4f14c7bdb4c2a3a8f609c65b8b
Konstantin Aksenov <aksenov.kostya@gmail.com>
2023-02-13 21:52:08 +1000

    Update googleplaycli.rb

--- Formula/googleplaycli.rb
+++ Formula/googleplaycli.rb
@@ -4,8 +4,6 @@ class Googleplaycli < Formula
   url "https://github.com/Vacxe/google-play-cli-kt/releases/download/0.2.0/google-play-cli-0.2.0.zip"
   sha256 "598538e433a21a2a0f0f32b9928697832a5f37834ca74f7f66f1cb08a94343ea"
 
-  bottle :unneeded
-
   depends_on "openjdk@11"
 
   def install
