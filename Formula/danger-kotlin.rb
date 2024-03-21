4767d67ac4319ea4e2a58680b14d01783e296574
Konstantin Aksenov <aksenov.kostya@gmail.com>
2024-03-21 19:48:29 +1000

    Update danger-kotlin.rb

--- Formula/danger-kotlin.rb
+++ Formula/danger-kotlin.rb
@@ -3,7 +3,7 @@ class DangerKotlin < Formula
   homepage "https://github.com/danger/kotlin"
   version "1.3.0" 
   url "https://github.com/Vacxe/kotlin/releases/download/#{version}/danger-kotlin-macosArm64.tar"
-  sha256 "fcf6562dab77c060e4ffc04e981a1ab54b7ac6855a7c97d4a081772494e2ef7a"
+  sha256 "0da105f8ed3d8c56cc0f924ec94a4b60c218b2326c602320090f7ec5657ff8af"
   head "https://github.com/danger/kotlin.git"
 
   # Use the vendored danger
