d3247683988e5b5e145a225abb5060dcbcf25ce0
Konstantin Aksenov <aksenov.kostya@gmail.com>
2024-05-01 10:51:14 +1000

    Update googleplaycli.rb

--- Formula/googleplaycli.rb
+++ Formula/googleplaycli.rb
@@ -1,8 +1,8 @@
 class Googleplaycli < Formula
   desc "Command line interface for Google play"
   homepage "https://github.com/Vacxe/google-play-cli-kt"
-  url "https://github.com/Vacxe/google-play-cli-kt/releases/download/0.4.3/google-play-cli.tar"
-  sha256 "b361a22ffe04c73f76af1fcfbbed4c389fe2e9d5dcc9df6057dfc11c5cab9bbb"
+  url "https://github.com/Vacxe/google-play-cli-kt/releases/download/0.4.4/google-play-cli.tar"
+  sha256 "2f53e7af544449a0527923be86e9d0c831ba298c86c19a75bb672034bdc09af7"
 
   depends_on "openjdk@11"
 
